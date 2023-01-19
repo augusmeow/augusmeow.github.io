c5faf9cd6e3d780d0644631f88ee292650634c25
AugusMeow <34206897+DrAugus@users.noreply.github.com>
2023-01-19 03:40:03 +0000

    initial

---
+++ src/.vitepress/config.ts
@@ -0,0 +1,33 @@
+import { defineConfig } from 'vitepress'
+
+export default defineConfig({
+    // These are app level configs.
+    title: 'Pilot',
+    description: 'Augus All Site',
+    base: "/",
+    lastUpdated: true,
+    cleanUrls: 'without-subfolders',
+    head: [
+        ['link', {
+            rel: 'icon',
+            href: '/logo.svg',
+        }],
+        ['meta', { name: 'theme-color', content: '#3c8772' }],
+    ],
+    themeConfig: {
+        // sidebar: sidebar,
+        socialLinks: [
+            { icon: 'github', link: 'https://github.com/DrAugus/' },
+            { icon: 'github', link: 'https://github.com/augusmeow/' },
+        ],
+        editLink: {
+            pattern: 'https://github.com/augusmeow/augusmeow.github.io/tree/master/src/:path',
+            text: 'Suggest changes to this page',
+        },
+        footer: {
+            message: 'Released under the GNU License.',
+            copyright: 'Copyright © 2023-present Augus'
+        },
+
+    }
+})
---
+++ src/index.md
@@ -0,0 +1,27 @@
+---
+layout: home
+
+title: 网站导航
+titleTemplate: Augus 的全部网站
+hero:
+  name: 网站导航
+  text: Augus 的全部网站
+  tagline: ""
+  actions:
+    - theme: brand
+      text: game
+      link: /game/
+    - theme: alt
+      text: notes
+      link: /docus/
+# Could add features description here
+features:
+  - title: 游戏
+    details: 当然是原神啦
+  - title: 笔记
+    details: 有很多笔记
+  - title: 编程
+    details: 各种编程
+  - title: 文科
+    details: 简单了解一下
+---
